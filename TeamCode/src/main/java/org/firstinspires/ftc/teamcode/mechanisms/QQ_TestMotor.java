a394060c3a56d4f3fe65a9713cd0b2534276302d
Nithya Golla <nbgolla@gmail.com>
2020-09-21 20:34:42 -0400

    added QQ test motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/QQ_TestMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/QQ_TestMotor.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
-public class QQ_TestMotor {
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class QQ_TestMotor extends QQ_Test{
+    private DcMotor dcMotor;
+    private double speed;
+
+
+    QQ_TestMotor(String desciption, double speed, DcMotor motor){
+
+        super(desciption);
+        this.speed = speed;
+        dcMotor = motor;
+
+    }
+
+    @Override
+    void run(boolean on, Telemetry telemetry) {
+        if(on){
+
+            dcMotor.setPower(speed);
+        } else{
+
+            dcMotor.setPower(0.0);
+        }
+        telemetry.addData("Encoder", dcMotor.getCurrentPosition());
+    }
 }
