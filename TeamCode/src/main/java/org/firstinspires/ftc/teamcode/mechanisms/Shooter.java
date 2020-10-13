fe98fa86f8ebe850981f2d0750586233dd85bb26
preetithirukonda <preetithirukonda@gmail.com>
2020-10-12 20:46:39 -0400

    coded wobbly goal, and changed test servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Shooter.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.mechanisms.tests.QQ_Test;
@@ -7,9 +8,12 @@ import org.firstinspires.ftc.teamcode.mechanisms.tests.QQ_Test;
 import java.util.List;
 
 class Shooter implements QQ_Mechanism {
+private DcMotor ShooterLeft;
+        private DcMotor ShooterRight;
     @Override
     public void init(HardwareMap hwMap) {
-
+ShooterLeft = hwMap.get(DcMotor.class, "LeftMotor");
+ShooterRight = hwMap.get(DcMotor.class, "RightMotor");
     }
 
     @Override
@@ -17,6 +21,19 @@ class Shooter implements QQ_Mechanism {
         return null;
     }
 
+    //create methods to set angle up and down, left, right
+public void ShooterUp(){
+
+}
+public void ShooterLeft(){
+
+}
+public void ShooterRight(){
+
+}
+public void ShooterDown(){
+
+}
     @Override
     public String getName() {
         return "Shooter";
