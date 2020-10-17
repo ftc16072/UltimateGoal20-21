494983195bbb03559b52e1bc7f63c62b0c1f677c
Nithya Golla <nbgolla@gmail.com>
2020-10-17 12:00:56 -0400

    Added RobotPose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Navigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Navigation.java
@@ -3,22 +3,56 @@ package org.firstinspires.ftc.teamcode.mechanisms;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.teamcode.mechanisms.tests.QQ_Test;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.utils.RobotPose;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Navigation {
+    // Public Classes
+    public RobotPose currentPosition;
     public BNO055IMU imu;
-    Robot robot;
+    // Private Classes
+    MecanumDrive mecanumDrive;
 
-    Navigation(Robot robot){
-        this.robot = robot;
+    // Private Values
+    private double angleTolerance = AngleUnit.RADIANS.fromDegrees(2);
+
+
+    Navigation(MecanumDrive mecanumDrive){
+        this.mecanumDrive = mecanumDrive;
     }
 
     void init(HardwareMap hwmap){
         imu = hwmap.get(BNO055IMU.class, "imu");
     }
+
+    public RobotPose getCurrentPosition(){
+        return currentPosition;
+    }
+
+
+    public boolean translateTo(RobotPose desiredPose, double distanceTolerance, DistanceUnit du){
+
+        //todo write drive code here
+
+        return currentPosition.getTransDistance(desiredPose, du).getR() < distanceTolerance;
+
+    }
+
+    public boolean turnTo(RobotPose desiredPose, double angleTolerance, AngleUnit au){
+
+        //todo write turn code here
+
+        return currentPosition.getAngleDistance(desiredPose, au) < angleTolerance;
+    }
+
+    public boolean driveTo(RobotPose desiredPose){
+        return translateTo(desiredPose, 2, DistanceUnit.CM) && turnTo(desiredPose, 2, AngleUnit.DEGREES);
+    }
+
+
+
+
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotPose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotPose.java
@@ -19,6 +19,26 @@ public class RobotPose {
         y_cm = du.toCm(y);
     }
 
+    public double getX(DistanceUnit du) {
+        return du.fromCm(x_cm);
+    }
+
+    public double getY(DistanceUnit du) {
+        return du.fromCm(y_cm);
+    }
+
+    public double getAngle(AngleUnit au) {
+        return au.fromRadians(theta);
+    }
+
+    public Polar getTransDistance(RobotPose otherPoint, DistanceUnit du){
+        return new Polar(otherPoint.getX(du) - getX(du), otherPoint.getY(du) - getY(du));
+    }
+
+    public double getAngleDistance(RobotPose otherPoint, AngleUnit au){
+        return otherPoint.getAngle(au) - getAngle(au);
+    }
+
 
 
 }
