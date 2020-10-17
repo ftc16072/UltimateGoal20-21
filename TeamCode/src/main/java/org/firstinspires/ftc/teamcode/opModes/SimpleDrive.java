cc365397b98b26ffe7432417df6ac0a256eb08c9
Nithya Golla <nbgolla@gmail.com>
2020-10-16 22:30:15 -0400

    Added QQ_Action and switched simple drive to use it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/SimpleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/SimpleDrive.java
@@ -2,19 +2,14 @@ package org.firstinspires.ftc.teamcode.opModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.actions.TeleopDriveAction;
+
 @TeleOp(name="Simple Drive")
 public class SimpleDrive extends QQ_Opmode {
     @Override
     public void init(){
         super.init();
         usesGamepads = true;
-    }
-
-    @Override
-    public void loop() {
-        super.loop();
-        robot.mecanumDrive.driveMecanum(qq_gamepad1.leftStick.getY(),
-                                        qq_gamepad1.leftStick.getX(),
-                                        qq_gamepad1.rightStick.getX());
+        currentAction = new TeleopDriveAction();
     }
 }
