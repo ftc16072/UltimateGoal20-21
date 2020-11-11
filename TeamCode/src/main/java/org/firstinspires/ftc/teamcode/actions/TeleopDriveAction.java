d75097fd055e404c01232c9848bfef39c2b7d866
Preeti Thirukonda <preeti.thirukonda@gmail.com>
2020-11-10 21:07:24 -0500

    added a call to manipulator controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actions/TeleopDriveAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actions/TeleopDriveAction.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.actions;
 
-import org.firstinspires.ftc.teamcode.mechanisms.Intake;
-import org.firstinspires.ftc.teamcode.mechanisms.Transfer;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.mechanisms.Intake;
 import org.firstinspires.ftc.teamcode.mechanisms.Transfer;
@@ -24,13 +22,7 @@ public class TeleopDriveAction extends QQ_Action {
      */
     public QQ_Action run(QQ_Opmode opmode) {
         driverControls(opmode);
-        //opmode.robot.nav.driveFieldRel(opmode.qq_gamepad1.leftStick, opmode.qq_gamepad1.rightStick.getX());
-
-        // Gamepad commands:
-        // Intake and tranfer on while holding, when released turn intake off and (amount of time) later turn transfer off.
-        // Fire command
-        // Pivot command
-        // Wobbly goal junk
+        manipulatorControls(opmode);
         return this;
     }
 
@@ -90,31 +82,31 @@ public class TeleopDriveAction extends QQ_Action {
     }
 
 
-    double rotateFromTrigger(double trigger){
+    double rotateFromTrigger(double trigger) {
         return trigger / 2;
     }
 
     public void driverControls(QQ_Opmode opmode) {
         opmode.qq_gamepad1.leftStick.setSquared(true);
 
-        if(opmode.qq_gamepad1.leftBumper()){
+        if (opmode.qq_gamepad1.leftBumper()) {
             opmode.robot.mecanumDrive.setMaxSpeed(SLOW_SPEED);
-        } else if(opmode.qq_gamepad1.rightBumper()) {
+        } else if (opmode.qq_gamepad1.rightBumper()) {
             opmode.robot.mecanumDrive.setMaxSpeed(MAX_SPEED);
-        }else {
+        } else {
             opmode.robot.mecanumDrive.setMaxSpeed(NORMAL_SPEED);
         }
 
-        if(opmode.qq_gamepad1.leftTrigger() >= 0.05){
+        if (opmode.qq_gamepad1.leftTrigger() >= 0.05) {
             opmode.robot.nav.driveRotate(-rotateFromTrigger(opmode.qq_gamepad1.leftTrigger()));
-        }else if(opmode.qq_gamepad1.rightTrigger() >= 0.05){
+        } else if (opmode.qq_gamepad1.rightTrigger() >= 0.05) {
             opmode.robot.nav.driveRotate(rotateFromTrigger(opmode.qq_gamepad1.rightTrigger()));
-        }else if(opmode.qq_gamepad1.rightStick.getR() >= 0.8){
+        } else if (opmode.qq_gamepad1.rightStick.getR() >= 0.8) {
             opmode.robot.nav.driveFieldRelativeAngle(
                     opmode.qq_gamepad1.leftStick.getPolar(),
                     opmode.qq_gamepad1.rightStick.getTheta(AngleUnit.RADIANS),
                     AngleUnit.RADIANS);
-        }else{
+        } else {
             opmode.robot.nav.driveFieldRel(
                     opmode.qq_gamepad1.leftStick, 0.0);
         }
