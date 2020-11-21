42486f7046f65e30cd057f5da9ed44234955f19a
Preeti Thirukonda <preeti.thirukonda@gmail.com>
2020-11-21 16:00:07 -0500

    Changed to use START_Y

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/QQ_ParkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/QQ_ParkAuto.java
@@ -14,7 +14,7 @@ public class QQ_ParkAuto extends QQ_Opmode {
     double START_X = 48;
     double TOLERANCE = 0.5;
     double START_Y = 9;
-    double PARK_Y = PARK_Y+24;
+    double PARK_Y = START_Y + 24;
     @Override
     public void init() {
         super.init();
