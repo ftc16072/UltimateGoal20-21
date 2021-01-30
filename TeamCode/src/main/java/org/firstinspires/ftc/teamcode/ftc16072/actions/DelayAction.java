4ea127a2ccf4ff5e65fde2cdfdf9b98d5844f8cf
tejasule <teja.sule@gmail.com>
2021-01-30 22:12:14 +0100

    added javadoc comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/actions/DelayAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/actions/DelayAction.java
@@ -13,7 +13,7 @@ public class DelayAction extends QQ_Action {
     /**
      *
      * @param opmode this gives the action access to our robot, gamepads, time left etc
-     * @return next action to run once finished
+     * @return next action to run once finishe
      */
 
     @Override
