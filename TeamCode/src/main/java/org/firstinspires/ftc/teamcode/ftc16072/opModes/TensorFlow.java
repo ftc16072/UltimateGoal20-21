3e23fbaedfca7933d27d56cb997603eaac95328f
tejasule <teja.sule@gmail.com>
2021-04-06 02:18:22 +0200

    vuforia

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/opModes/TensorFlow.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode.ftc16072.opModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.R;
+import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.QQ_Mechanism;
+import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.tests.QQ_Test;
+import org.firstinspires.ftc.teamcode.ftc16072.utils.QQ_BothGamepad;
+
+import java.util.List;
+
+@TeleOp()
+public class TensorFlow extends QQ_Opmode {
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+
+    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Quad";
+    private static final String LABEL_SECOND_ELEMENT = "Single";
+
+    @Override
+    public void init() {
+        super.init();
+        initVuforia();
+        initTensorFlowObjectDetector();
+
+    }
+
+    @Override
+    public void loop() {
+        if (tfod != null) {
+            // getUpdatedRecognitions() will return null if no new information is available since
+            // the last time that call was made.
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                telemetry.addData("# Object Detected", updatedRecognitions.size());
+            }
+            else {
+                telemetry.addData("# Object Detected", 0);
+            }
+        }
+
+    }
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = hardwareMap.appContext.getString(R.string.vuforiaLicense);
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+
+
+    }
+
+    private void initTensorFlowObjectDetector() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+
+        if (tfod != null) {
+            tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 16/9).
+            tfod.setZoom(2.5, 16.0 / 9.0);
+
+        }
+
+    }
+
+
+}
+
